215b7e96e7e9094d3d3af07934eef4ed44830c69
Your Name <you@example.com>
2022-07-10 14:55:23 +0900

    Camera,Run,Rotate

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //스피드 조정 변수
+    [SerializeField] private float walkSpeed;
+    //Inspector창에서 값을 수정할 수 있다
+    [SerializeField] private float runSpeed;
+    private float applySpeed;
+
+    //상태 변수
+    private bool isRun = false;
+
+    //민감도
+    [SerializeField] private float lookSensitivity;
+
+    //카메라 한계
+    [SerializeField] private float cameraRotationLimit;
+    private float currentCameraRotationX = 0f;
+
+    //필요한 컴포넌트
+    [SerializeField] private Camera theCamera;
+    private Rigidbody myRigid;
+    //Collider에 물리적 요소를 입힌다
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //theCamera = FindObjectOfType<Camera>();
+        myRigid = GetComponent<Rigidbody>();
+        applySpeed = walkSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TryRun(); // Move함수 위
+        Move();
+        CameraRotation();
+        CharacterRotation();
+    }
+    private void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    private void Running()
+    {
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
+
+    private void Move()
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _moveDirX;
+        //(1, 0, 0) * (1 or -1 or 0)
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
+        //(0, 0, 1)
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
+        // 벡터의 합 (x,0,0) + (0,0,z)
+        // normalized 는 합을 1로 만드는 것을 권장한다
+        // (1,0,1) = 2 
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+        //
+    }
+    private void CharacterRotation()
+    {
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
+        //오일러 벡터값을 쿼터니언으로 바꾼다
+        //Debug.Log(myRigid.rotation);
+        //Debug.Log(myRigid.rotation.eulerAngles);
+    }
+    private void CameraRotation()
+    {
+        //상하 카메라 회전
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotationX = _xRotation * lookSensitivity;
+        currentCameraRotationX += _cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+}
